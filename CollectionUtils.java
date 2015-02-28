590180ce2c05a20ceca3e8bf5687a857eb062884
vishnumishra <vishnu143mishra@gmail.com>
2015-02-28 12:32:03 +0530

    v.1 is completed

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -1,4 +1,13 @@
+import java.util.*;
 
 public  class CollectionUtils{
 	
+	public static List<Integer> map(List<Integer> list, ListMapper method){
+		List<Integer> resultList = new ArrayList<Integer>();
+		for (Integer i:list ) {
+			resultList.add((int) method.incrementByOne(i));
+		}
+		return resultList; 
+	}
+
 }
--- CollectionUtilsTest.java
+++ CollectionUtilsTest.java
@@ -1,4 +1,28 @@
+import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 
+interface ListMapper{
+	public int incrementByOne(int num);
+}
+
+class Mapper implements ListMapper {
+	public int incrementByOne(int number){
+		return number+1;
+	};
+}
+
+public class CollectionUtilsTest{
+
+	@Test
+	public void list_mapper_increment_the_number_by_one(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(5);
+		list.add(15);
+		list.add(45);
+		ListMapper mapper = new Mapper();
+		List<Integer> list_1 = CollectionUtils.map(list,mapper);
+		assertEquals(6,(int)list_1.get(0));
+	}
+}
