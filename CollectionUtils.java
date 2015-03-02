fa322c8ce5a409123648891280e3d15b8bb5307f
vishnumishra <vishnu143mishra@gmail.com>
2015-03-02 09:16:00 +0530

    collection util v.3 is completed

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
-public  class CollectionUtils{
-	
-	public static <E> List<E> map(List<E> list, ListMapper method){
-		List<E> resultList = new ArrayList();
+public  class CollectionUtils{				
+	public static <E,K> List<K> map(List<E> list, ListMapper<E,K> method){ //or use ListMapper without <E,K> 
+		List<K> resultList = new ArrayList<K>();
 		for (E i:list ) {
-			resultList.add((E)method.increment(i));
+			resultList.add((K)method.increment(i));
 		}
 		return resultList; 
 	}
@@ -16,5 +15,20 @@ public  class CollectionUtils{
 			 resultList.add(i);
 		}
 		return resultList; 
+	}
+	public static <E,K> K reducer(List<E> list, ListReducer<E,K> method , K initial){
+		K result =null ;
+		// result =  method.sqrtAdder(initial,);
+		// for (int i=0;i<list.lenght;i++ ) {
+		// 	if(i==0)
+		// 		result = initial;
+		// 	result =  method.sqrtAdder(result,i);
+		// }
+		for (E i:list ) {
+			if(result == null)
+				result = initial;
+			result =  method.sqrtAdder(result,i);
+		}
+		return result;
 	}
 }
--- CollectionUtilsTest.java
+++ CollectionUtilsTest.java
@@ -3,13 +3,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-interface ListMapper<E>{
-	public E increment(E num);
+interface ListMapper<E,K>{
+	public K increment(E num);
 }
 
 interface ListFilter<E>{
 	public boolean filterLess(E num);
 }
+
+interface ListReducer<E,K>{
+	public K sqrtAdder(K previous,E curren);
+}
+
+class Reducer implements ListReducer<Integer,Double> {
+	public Double sqrtAdder(Double previous,Integer current){
+		return previous+Math.sqrt(current);
+	};
+}
+
 
 class FilterInt implements ListFilter<Integer> {
 	public boolean filterLess(Integer number){
@@ -23,18 +34,23 @@ class FilterString implements ListFilter<String> {
 	};
 }
 
-class MapInt implements ListMapper<Integer> {
+class MapInt implements ListMapper<Integer,Integer> {
 	public Integer increment(Integer number){
 		return number+1;
 	};
 }
 
-class MapString implements ListMapper<String> {
+class SquareRoot implements ListMapper<Integer,Double> {
+	public Double increment(Integer number){
+		return Math.sqrt(number);
+	};
+}
+
+class MapString implements ListMapper<String,String> {
 	public String increment(String str){
 		return str+" incremented";
 	};
 }
-
 
 public class CollectionUtilsTest{
 
@@ -49,16 +65,30 @@ public class CollectionUtilsTest{
 		assertEquals(6,(int)list_1.get(0));
 	}
 	@Test
+	public void list_mapper_the_sqrt_of_the_number(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(5);
+		list.add(15);
+		list.add(45);
+		ListMapper mapper = new SquareRoot();
+		List<Double> list_1 = CollectionUtils.map(list,mapper);
+		assertEquals((double)Math.sqrt(5),(double)list_1.get(0),0);
+		assertEquals((double)Math.sqrt(15),(double)list_1.get(1),0);
+		assertEquals((double)Math.sqrt(45),(double)list_1.get(2),0);
+	}
+
+	@Test
 	public void list_mapper_add_incremented_to_string(){
 		List<String> list = new ArrayList<String>();
 		list.add("param");
 		list.add("karam");
 		list.add("dhram");
 		ListMapper mapper = new MapString();
-		List<String> list_1 = CollectionUtils.<String>map(list,mapper);
+		List<String> list_1 = CollectionUtils.map(list,mapper);
 		assertEquals("param incremented",(String)list_1.get(0));
 		assertEquals("karam incremented",(String)list_1.get(1));
 	}
+
 	@Test
 	public void list_filter_remove_the_number_less_then_10(){
 		List<Integer> list = new ArrayList<Integer>();
@@ -80,5 +110,16 @@ public class CollectionUtilsTest{
 		List<String> list_1 = CollectionUtils.filter(list,filterObj);
 		assertEquals("parmatma",(String)list_1.get(0));
 		assertEquals("vishnu",(String)list_1.get(1));
+	}
+
+
+	@Test
+	public void list_reduce_reduce_the_list_and_give_the_resualt(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(5);
+		list.add(8);
+		Reducer reduceObj = new Reducer();
+		double result = CollectionUtils.reducer(list,reduceObj,Math.sqrt(3));
+		assertEquals((double)(Math.sqrt(3)+Math.sqrt(5)+Math.sqrt(8)),result,0);
 	}
 }
